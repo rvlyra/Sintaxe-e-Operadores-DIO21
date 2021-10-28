02f90090ea6727876b36dc9256280f28fc75db6f
Rubens Lyra <rubensvlyra@gmail.com>
2021-10-28 11:41:21 -0300

    initializing

---
+++ operadores-n01.js
@@ -0,0 +1,44 @@
+/* 
+	Operadores de atribuição
+*/
+
+// comparação (=) //
+
+var a = 2
+var b = 2
+
+console.log(a+b)
+
+/* 
+	Operadores de comparação
+*/
+
+// comparação (==) e (===) //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
